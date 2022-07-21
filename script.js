de873614c0e2d2da9d2afce758fabcdc3c665511
tomriddle777 <tommy.lu112358@gmail.com>
2022-07-20 21:50:53 -0700

    Minor JS cleanups and CSS edits

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createGrid (size, original) {
   const divsContainer = document.querySelector('.divs-container');
 
   if (newSize != 0){
-    const rowDiv = document.querySelectorAll('.row-div')
+    const rowDiv = document.querySelectorAll('.row-div');
     for (let i = 0; i < original; i++) {
       divsContainer.removeChild(rowDiv[i]);
     }
@@ -43,7 +43,65 @@ function createGrid (size, original) {
   }
 
   const rowDiv = document.querySelectorAll('.row-div');
-  let rowArray = []
+  let rowArray = [];
+
+  rowDiv.forEach(row => rowArray.push(row));
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      divsGrid[i][j].addEventListener('mouseover', () => divsGrid[i][j].classList.add('changed'));
+      rowArray[i].appendChild(divsGrid[i][j]);
+    }
+  }
+}
+
+/*
+let originalSize = 16, newSize = 0;
+
+createGrid(originalSize, newSize);
+
+button = document.querySelector('button');
+button.addEventListener('click', () => {
+  let answer = prompt("Enter amount of squares per side (max. 100): ");
+  if (answer && answer <= 100) {
+    newSize = answer;
+  } else {
+    alert("ERROR");
+  }
+  createGrid(newSize, originalSize);
+  originalSize = newSize;
+})
+
+function createGrid (size, original) {
+  let divsRow = [],  divsGrid = [], rowDivider = [];
+  const divsContainer = document.querySelector('.divs-container');
+
+  if (newSize != 0){
+    const rowDiv = document.querySelectorAll('.row-div');
+    for (let i = 0; i < original; i++) {
+      divsContainer.removeChild(rowDiv[i]);
+    }
+  }
+
+  rowDivider = [];
+
+  for (let i = 0; i < size; i++) {
+    divsRow = [];
+    rowDivider[i] = document.createElement('div');
+    rowDivider[i].classList.add('row-div');
+    for (let j = 0; j < size; j++) {
+      divsRow[j] = document.createElement('div');
+      divsRow[j].classList.add('square', `row-${i + 1}`, `col-${j + 1}`);
+    }
+    divsGrid[i] = divsRow;
+  }
+
+  for (let i = 0; i < size; i++) {
+    divsContainer.appendChild(rowDivider[i]);
+  }
+
+  const rowDiv = document.querySelectorAll('.row-div');
+  let rowArray = [];
 
   rowDiv.forEach(row => rowArray.push(row));
 
@@ -58,3 +116,4 @@ function createGrid (size, original) {
     this.classList.add('changed');
   }
 }
+*/
